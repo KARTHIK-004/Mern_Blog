4d6c59bf2145ac3ef3cbbbcc898945e62ed6baff
Alpha <121655660+KARTHIK-004@users.noreply.github.com>
2024-02-04 12:58:38 +0530

    complete update user profile page functionality

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -5,7 +5,6 @@ const initialState = {
   error: null,
   loading: false,
 };
-
 const userSlice = createSlice({
   name: 'user',
   initialState,
@@ -23,9 +22,29 @@ const userSlice = createSlice({
       state.loading = false;
       state.error = action.payload;
     },
+    updateStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    updateSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    updateFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
   },
 });
 
-export const { signInStart, signInSuccess, signInFailure } = userSlice.actions;
+export const {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+  updateStart,
+  updateSuccess,
+  updateFailure,
+} = userSlice.actions;
 
 export default userSlice.reducer;
